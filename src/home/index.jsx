e58c24c37b3e420e4d2d7c27b1ac77cc7137015a
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-03-07 17:58:53 +0530

    fixed typing animation

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -7,9 +7,26 @@ import { useUser } from '@clerk/clerk-react'
 import { useContext } from 'react'
 import { ThemeContext } from '@/context/ThemeContext'
 import { CarouselSize } from '@/components/custom/CarouselSize'
-
+import { useEffect, useState } from 'react'
 
 function Home() {
+    const fullText = '"Leverage the potential of AI to set yourself apart from the competition."';
+    const [displayedText, setDisplayedText] = useState('');
+
+    useEffect(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+            if (index < fullText.length) {
+                setDisplayedText(fullText.slice(0, index + 1));
+                index++;
+            } else {
+                clearInterval(interval);
+            }
+        }, 18); // Adjust speed as needed
+
+        return () => clearInterval(interval);//clean up function to clear interval returned in useEffect
+    }, []);
+
     const { isSignedIn } = useUser();
     const { theme, setTheme } = useContext(ThemeContext);
     return (
@@ -20,7 +37,7 @@ function Home() {
                     <div className='flex flex-col gap-2 mt-10 place-items-center'>
                         <h1 className='font-serif text-center mb-10'>Welcome to <span className={(theme === 'light') ? 'bg-gradient-to-r from-violet-400 to-indigo-600 p-2 border rounded-xl text-white text-2xl sm:text-5xl whitespace-nowrap shadow-xl shadow-primary' : 'p-2 border-2 border-white rounded-xl text-white text-2xl sm:text-5xl whitespace-nowrap shadow-xl shadow-[rgba(0,191,255,0.8)]'}>Kenshi Resumes</span> </h1>
                         <div>
-                            <h3 className={(theme === 'light') ? 'text-center font-medium text-xs sm:text-base typing' : 'text-center font-medium text-xs sm:text-base typing sm:whitespace-nowrap'}>"Leverage the potential of AI to set yourself apart from the competition."</h3>
+                            <h3 className={(theme === 'light') ? 'text-center font-medium text-xs sm:text-base' : 'text-center font-medium text-xs sm:text-base'}>{displayedText}</h3>
                         </div>
                         <div className='flex justify-center'>
                             <Link to={isSignedIn ? '/dashboard' : '/auth/sign-in'}>
