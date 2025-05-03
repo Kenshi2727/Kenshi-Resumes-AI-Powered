2bafd7b8aa8c8fd5acaa8c0be20d8860a867baa2
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-05-03 06:58:20 +0530

    fixed ats frontend for mobile

--- src/ats_score/[documentId]/index.jsx
+++ src/ats_score/[documentId]/index.jsx
@@ -7,7 +7,7 @@ function ATS_Score() {
     const { theme } = useContext(ThemeContext);
     return (
         <div className={(theme === 'light') ? 'min-h-screen flex items-center justify-center bg-gradient-to-r from-red-200 to-yellow-200' : 'min-h-screen flex items-center justify-center'}>
-            <div className={(theme === 'light') ? 'bg-white p-7 rounded-lg shadow-lg w-full max-w-4xl md:h-[500px] flex flex-col md:grid md:grid-cols-2 gap-4' : 'p-7 rounded-lg shadow-lg shadow-[rgba(0,191,255,0.8)] w-full max-w-4xl md:h-[500px] flex flex-col md:grid md:grid-cols-2 gap-4'}>
+            <div className={(theme === 'light') ? 'bg-white p-7 rounded-lg shadow-lg md:w-full md:max-w-4xl max-w-[21rem] md:h-[500px] flex flex-col md:grid md:grid-cols-2 gap-4' : 'p-7 rounded-lg shadow-lg shadow-[rgba(0,191,255,0.8)] w-full max-w-4xl md:h-[500px] flex flex-col md:grid md:grid-cols-2 gap-4'}>
                 <Score />
                 <Recommendations />
             </div>
